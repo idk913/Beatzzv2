b259f8a880370fc9b4418c851b17a8752ea2d3fc
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-18 12:23:53 +0000

    pylint: auto fixes

--- userbot/plugins/log_chats.py
+++ userbot/plugins/log_chats.py
@@ -5,10 +5,12 @@ import logging
 import os
 import sys
 from asyncio import sleep
+
 from telethon import events
+
+from ..utils import admin_cmd
 from . import CMD_HELP
 from .afk import USERAFK_ON
-from ..utils import admin_cmd
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.WARN
@@ -96,6 +98,7 @@ async def set_no_log_p_m(event):
                 await event.edit("Won't Log Messages from this chat")
                 await asyncio.sleep(3)
 
+
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.mentioned))
 async def log_tagged_messages(event):
     if not USERAFK_ON and not (await event.get_sender()).bot:
@@ -107,6 +110,7 @@ async def log_tagged_messages(event):
                     Config.PM_LOGGR_BOT_API_ID,
                     f"#TAGS \nhttps://t.me/c/{hmm.id}/{event.message.id}",
                 )
+
 
 CMD_HELP.update(
     {
