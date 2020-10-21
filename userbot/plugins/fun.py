e91e52a0ea73fe1d07fb1b9b470df14e71a62251
idk913 <68418175+idk913@users.noreply.github.com>
2020-10-21 18:07:16 +0530

    Update fun.py

--- userbot/plugins/fun.py
+++ userbot/plugins/fun.py
@@ -1,7 +1,7 @@
-"""COMMAND : .join , .pay , .work , .push , .aag , .climb"""
+"""COMMAND : .join , .pay , .work , .push , .aag , .climb, .m1, .g1, .ftext"""
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @borg.on(admin_cmd(pattern="join$"))
@@ -104,3 +104,167 @@ async def _(event):
     else:
         await event.reply(mentions)
     await event.delete()
+    
+@bot.on(admin_cmd(outgoing=True, pattern="g1 ?(.*)"))
+@bot.on(sudo_cmd(pattern="g1 ?(.*)", allow_sudo=True))
+async def payf(event):
+    paytext = event.pattern_match.group(1)
+    pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+        paytext * 1,
+    )
+    await edit_or_reply(event, pay)
+    
+    
+@bot.on(admin_cmd(pattern="ftext (.*)"))
+@bot.on(sudo_cmd(pattern="ftext (.*)", allow_sudo=True))
+async def payf(event):
+    paytext = event.pattern_match.group(1)
+    pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
+        paytext * 8,
+        paytext * 8,
+        paytext * 2,
+        paytext * 2,
+        paytext * 2,
+        paytext * 6,
+        paytext * 6,
+        paytext * 2,
+        paytext * 2,
+        paytext * 2,
+        paytext * 2,
+        paytext * 2,
+    )
+    await edit_or_reply(event, pay)
+    
+@bot.on(admin_cmd(pattern="ml (.*)"))
+@bot.on(sudo_cmd(pattern="ml (.*)", allow_sudo=True))
+async def kakashi(beats):
+    message = beats.pattern_match.group(1)
+    await edit_or_reply(
+        beats,
+        "`\n█████████`"
+        "`\n█▄█████▄█`"
+        "`\n█▼▼▼▼▼`"
+        f"`\n█  {message}`"
+        "`\n█▲▲▲▲▲`"
+        "`\n█████████`"
+        "`\n ██   ██`",
+    )
